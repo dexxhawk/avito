904fed0d17097fa91ee859cdaa3280073ef9c910
dexxhawk <Dsshnyakin@yandex.ru>
2024-09-16 02:44:12 +0300

    Change schema for bid

--- src/schemas/bid.py
+++ src/schemas/bid.py
@@ -1,25 +1,24 @@
+import datetime
 from uuid import UUID
 from pydantic import BaseModel, ConfigDict, Field
 from pydantic.alias_generators import to_camel
-from src.schemas.enums import BidStatus
+from src.schemas.enums import BidAuthorType, BidStatus
 
 
 class BidBase(BaseModel):
     name: str = Field(..., max_length=100)
     description: str = Field(..., max_length=500)
     status: BidStatus
-    tender_id: UUID
-    organization_id: UUID
-    creator_username: str
 
     model_config = ConfigDict(
         alias_generator=to_camel,
         populate_by_name=True
     )
 
-
 class BidCreate(BidBase):
-    pass
+    tender_id: UUID
+    organization_id: UUID
+    creator_username: str
 
 
 class BidUpdate(BaseModel):
@@ -28,6 +27,14 @@ class BidUpdate(BaseModel):
 
 
 class BidResponse(BidBase):
+    id: UUID
+    name: str
+    status: BidStatus
+    author_type: BidAuthorType
+    author_id: UUID
+    version: int
+    created_at: datetime.datetime 
+
     model_config = ConfigDict(
         alias_generator=to_camel,
         populate_by_name=True,
